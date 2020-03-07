7abaa2186d37c8ccc9904b3c7bc6c167ca57faeb
Hot-Tutorials <61880709+Hot-Tutorials@users.noreply.github.com>
2020-03-07 16:01:21 +0000

    Create Rainbow Background.java

---
+++ Stuff/Rainbow Background.java
@@ -0,0 +1,14 @@
+/**
+
+        FLASHING LIGHTS WARNING
+
+*/
+
+	public static int rainbowBG(int left, int top, int right, int bottom, int speed) {
+		Gui gui = new Gui();
+		Minecraft mc = Minecraft.getMinecraft();
+	  long time = System.currentTimeMillis() - (speed / 500);
+	  int color = Color.HSBtoRGB(time % (int) 10000.0F / 10000.0F, 0.8F, 0.7F);
+	  gui.drawRect(left, top, right, bottom, color);
+		return color;
+	}
